e48aee44c01be1d6fd2f08fbe362c18ad36b4370
2_ruthless <105881276+DaRuthlessPWR@users.noreply.github.com>
2024-02-01 13:28:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'Asia/Dhaka', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Europe/London', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -23,28 +23,28 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('1079010612769722508')
+    .setApplicationId('1202681824271142955')
     .setType('STREAMING')
-    .setURL('https://www.twitch.tv/nocopyrightsounds') //Must be a youtube video link 
-    .setState('The Ultimate Masterpiece of the White Room‌')
-    .setName('Kiyotaka Ayanokōji')
-    .setDetails(`THE NAME IT SHOWS YOUR STREAMING [${formatTime()}]`)
+    .setURL('https://twitch.tv/dapwr') //Must be a youtube video link 
+    .setState('The Game Of Life')
+    .setName('mrnekrozyt')
+    .setDetails(`♠♠♠ [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/868886105473298432/1201467592636899419/20240129_160232.gif') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Kiyotaka Ayanokōji') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1102093230906023946/1135219284071698562/HB_VerifyWhite.gif') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Verify') //Text when you hover the Small image
-    .addButton('Discord Server', 'https://discord.gg/PBED6yEXpT')
-    .addButton('Facebook Page', 'https://facebook.com/patkhet.lol');
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/968309668600496198/988828316968562728/36DB555A-F589-42EB-B43F-EC3A46D1E5CE.gif?ex=655ccd22&is=654a5822&hm=ad572076d7ca6408559923f44de548d9f3a8801496afbd9e877e48b1cf7276b4&=') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('DM TO GET ON') //Text when you hover the Large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1161404154669969501/1172745935751741540/ruthlessskullpfp.jpg?ex=65616fc3&is=654efac3&hm=a396d0c8319ad8d82fd5300427cb2c0567f54b05b2856cb3c517812ec38d45d9&=&width=418&height=450') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('.gg/grapes') //Text when you hover the Small image
+    .addButton('Who Is Ruthless?', 'https://guns.lol/ruthless')
+    .addButton('.gg/GRAPES', 'https://discord.gg/grapes');
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
+  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
 
   let prevTime = null;
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `ℭ𝔩𝔞𝔰𝔰𝔯𝔬𝔬𝔪 𝔬𝔣 𝔱𝔥𝔢 𝔈𝔩𝔦𝔱𝔢`;
+      const newDetails = `♠♠♠ [${newTime}]`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
